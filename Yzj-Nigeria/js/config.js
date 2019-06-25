1f28e9b446b2970cc9be63ee2142689ede73114e
lth1990118 <438168789@qq.com>
2019-06-25 23:55:41 +0800

    0625

---
+++ Yzj-Nigeria/js/config.js
@@ -0,0 +1,33 @@
+var config = {
+
+// 	url: "http://wskmlapi.yzj1999.com/api/ws/",
+// 	urlPic: "http://wskmlapi.yzj1999.com/api/pic/",
+// 	urlPicRoot: "http://wskmlapi.yzj1999.com/Images/",
+		url:"http://wsnrlyapi.yizhijiu.com/api/ws/" ,
+		urlPic:"http://wsnrlyapi.yizhijiu.com/api/pic/" ,
+		urlPicRoot:"http://wsnrlyapi.yizhijiu.com/Images/" ,
+	reqSys: 'EN'
+}
+
+var tempJson = {
+	menu: {
+		sale_EN: "我的销售",
+		agent_EN: "我的代理",
+		bonus_EN: "我的钱包",
+		purchase_EN: "我的采购",
+		loginPassword_EN: "登录密码",
+		cashPassword_EN: "提现密码",
+		info_EN: "我的信息",
+		exit_EN: "退出系统",
+		myTeam_EN: "我的团队"
+	},
+	system: "EN",
+	msgAgent: "Agent Added Successfully！",
+	msgSale: "Sales Added Successfully！",
+	msgNoData: "No more data.！",
+	msgBonus: "Successful submission of cash application！",
+	msgBonusForOther: "Successful submission of transfer application！",
+	msgInfoUp: "Successful revision of my information！",
+	msgLoginPasswordUp: "Successful modification of login password",
+	msgCashPasswordUp: "提现密码修改成功"
+}
