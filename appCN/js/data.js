3ff8087c7829ee29c6cba5c97af5b599c4219fea
lth1990118 <438168789@qq.com>
2019-07-28 23:53:36 +0800

    20190728

---
+++ appCN/js/data.js
@@ -0,0 +1,125 @@
+var tempJson = {
+	msgAgent: {
+		CN:"代理添加成功！",
+		FR:"Agent address success！"
+	},
+	msgSale:  {
+		CN:"销售添加成功！",
+		FR:"Succès des ventes！"
+	},
+	msgNoData: {
+		CN:"没有相关数据了！",
+		FR:"Il n’y a plus de données pertinentes！"
+	},
+	msgBonus: {
+		CN:"提现申请提交成功！",
+		FR:"succès！"
+	},
+	msgBonusForOther: {
+		CN:"转账申请提交成功！",
+		FR:"succès！"
+	},
+	msgInfoUp: {
+		CN:"我的信息修改成功！",
+		FR:"succès！"
+	},
+	msgLoginPasswordUp: {
+		CN:"登录密码修改成功！",
+		FR:"succès！"
+	},
+	msgCashPasswordUp: {
+		CN:"提现密码修改成功！",
+		FR:"succès！"
+	},
+	guide:{
+		close: {
+			CN:"立即体验！",
+			FR:"Expérience immédiate"
+		}
+	},
+	login:{
+		header:{
+			CN:"登录",
+			FR:"à"
+		},
+		accountLabel:{
+			CN:"账号",
+			FR:"compte"
+		},
+		accountPL:{
+			CN:"请输入账号",
+			FR:"Nom connecter"
+		},
+		passwordLabel:{
+			CN:"密码",
+			FR:"password"
+		},
+		passwordPL:{
+			CN:"请输入密码",
+			FR:"password"
+		},
+		login:{
+			CN:"登录",
+			FR:"Soumettre"
+		},
+		reg:{
+			CN:"注册",
+			FR:"Numéro de compte enregistré"
+		},
+		download:{
+			CN:"正在下载最新版本",
+			FR:"Je télécharge la dernière version"
+		}
+	}
+	,reg:{
+		header: {
+			CN:"注册",
+			FR:"l’enregistrement"
+		},
+		IntroIDLabel:{
+			CN:"推荐人ID",
+			FR:"Recom ID"
+		},
+		IntroIDPL:{
+			CN:"请输入推荐人ID",
+			FR:"Recom ID"
+		},
+		VerifyCodeLabel:{
+			CN:"验证码",
+			FR:"Validat"
+		},
+		VerifyCodePL:{
+			CN:"请输入验证码",
+			FR:"Validat"
+		},
+		UserNameLabel:{
+			CN:"登录名",
+			FR:"Nom"
+		},
+		UserNamePL:{
+			CN:"请输入登录名",
+			FR:"Nom connecter"
+		},
+		PwdLoginLabel:{
+			CN:"登陆密码",
+			FR:"Passe"
+		},
+		PwdLoginPL:{
+			CN:"请输入登陆密码",
+			FR:"Mot de passe"
+		},
+		PwdLoginConfirmLabel:{
+			CN:"确认密码",
+			FR:"Confirm"
+		},
+		PwdLoginConfirmPL:{
+			CN:"请输入确认密码",
+			FR:"Confirm passe"
+		},		
+		reg:{
+			CN:"注册",
+			FR:"Soumettre"
+		}
+	}
+}
+	
