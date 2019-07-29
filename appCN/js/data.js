0c42b7819d02961cd71c18efe023d1b9b600800d
lth1990118 <438168789@qq.com>
2019-07-29 23:37:53 +0800

    2019-07-29

--- appCN/js/data.js
+++ appCN/js/data.js
@@ -120,6 +120,252 @@ var tempJson = {
 			CN:"注册",
 			FR:"Soumettre"
 		}
+	},
+	head:{
+		header:{
+			CN:"头像",
+			FR:"tête"
+		},
+		scaleX_id:{
+			CN:"左右变换",
+			FR:"droite et gauche"
+		},
+		scaleY_id:{
+			CN:"上下变换",
+			FR:"en haut et en bas"
+		},
+		rotateMax_id:{
+			CN:"旋转90°",
+			FR:"tournoyer 90°"
+		},
+		confirm_id:{
+			CN:"确定并保存",
+			FR:"Identifier et préserver"
+		},
+		photo:{
+			CN:"拍照上传",
+			FR:"Photo téléchargée"
+		},
+		picture:{
+			CN:"从相册选择",
+			FR:"Choisis de l’album"
+		},
+		cancel:{
+			CN:"取消",
+			FR:"supprimer"
+		},
+		success:{
+			CN:"上传成功",
+			FR:"Téléchargement réussi"
+		},
+		error:{
+			CN:"上传头像出现错误，请稍后再试",
+			FR:"erreur"
+		},
+		noPic:{
+			CN:"没有选择图片",
+			FR:"Image non sélectionnée"
+		}
+	},
+	main:{
+		header:{
+			CN:"会员主页",
+			FR:"Member Page"
+		},
+		UserNameLabel:{
+			CN:"登录名",
+			FR:"Nom"
+		},
+		UserIDLabel:{
+			CN:"我的ID",
+			FR:"Mon ID"
+		},
+		VerifyCodeLabel:{
+			CN:"验证码",
+			FR:"Validat"
+		},
+		SumBonusLabel:{
+			CN:"收入",
+			FR:"Revenu"
+		},
+		LeftCashLabel:{
+			CN:"余额",
+			FR:"Solde"
+		},
+		UserLevelLabel:{
+			CN:"级别",
+			FR:"Grade"
+		},
+		LeftSellLabel:{
+			CN:"库存",
+			FR:"Stocks"
+		},		
+		agent:{
+			CN:"代理",
+			FR:"Agent"
+		},
+		sale:{
+			CN:"销售",
+			FR:"Vente"
+		},
+		bonus:{
+			CN:"钱包",
+			FR:"Argent"
+		},
+		menu:{
+			CN:"菜单",
+			FR:"Menu"
+		},
+		camera:{
+			CN:"拍照或录像",
+			FR:"Photo téléchargée"
+		},
+		picture:{
+			CN:"选取现有的",
+			FR:"Choisis de l’album"
+		},
+		cancel:{
+			CN:"取消",
+			FR:"supprimer"
+		}
+	},
+	agent:{
+		addAgent:{
+			CN:"添加代理",
+			FR:"Ajouter agent"
+		},
+		agentList:{
+			CN:"代理记录",
+			FR:"Enregistr"
+		},
+		loginNameLabel:{
+			CN:"登录名",
+			FR:"Nom"
+		},
+		loginName:{
+			CN:"登录名",
+			FR:"Nom connecter"
+		},
+		loginPasswordLabel:{
+			CN:"登录密码",
+			FR:"passe"
+		},
+		loginPassword:{
+			CN:"登录密码",
+			FR:"Mot de passe"
+		},
+		loginConfirmPasswordLabel:{
+			CN:"确认密码",
+			FR:"Confirm"
+		},
+		loginConfirmPassword:{
+			CN:"确认密码",
+			FR:"Confirm password"
+		},
+		date:{
+			CN:"日期",
+			FR:"Date"
+		},
+		agent:{
+			CN:"代理",
+			FR:"Agent"
+		},
+		level:{
+			CN:"级别",
+			FR:"Level"
+		}
+	},		
+	bonus:{
+		addAgent:{
+			CN:"添加代理",
+			FR:"Ajouter agent"
+		},
+		agentList:{
+			CN:"代理记录",
+			FR:"Enregistr"
+		},
+		loginNameLabel:{
+			CN:"登录名",
+			FR:"Nom"
+		},
+		loginName:{
+			CN:"登录名",
+			FR:"Nom connecter"
+		},
+		loginPasswordLabel:{
+			CN:"登录密码",
+			FR:"passe"
+		},
+		loginPassword:{
+			CN:"登录密码",
+			FR:"Mot de passe"
+		},
+		loginConfirmPasswordLabel:{
+			CN:"确认密码",
+			FR:"Confirm"
+		},
+		loginConfirmPassword:{
+			CN:"确认密码",
+			FR:"Confirm password"
+		},
+		date:{
+			CN:"日期",
+			FR:"Date"
+		},
+		agent:{
+			CN:"代理",
+			FR:"Agent"
+		},
+		level:{
+			CN:"级别",
+			FR:"Level"
+		}
+	},	
+	common:{
+		submit:{
+			CN:"提交",
+			FR:"Soumettre"
+		},
+		cancel:{
+			CN:"取消",
+			FR:"supprimer"
+		},
+		exitMsg:{
+			CN:"再按一次退出应用",
+			FR:"Appuyez à nouveau sur la touche pour sortir de l’application"
+		},
+		error:{
+			CN:"错误",
+			FR:"erreur"
+		},
+		passwordValidate:{
+			CN:"账号或密码不应小于6位数",
+			FR:"Le numéro de compte ou le mot de passe ne doit pas être inférieur à 6 chiffres"
+		},
+		passwordEqual:{
+			CN:"密码不一致",
+			FR:"Le mot de passe est entré deux fois incohérent"
+		},
+		passoraccountIll:{
+			CN:"账号或密码不应小于6位数",
+			FR:"Le numéro de compte ou le mot de passe ne doit pas être inférieur à six chiffres"			
+		},
+		accountIll:{
+			CN:"推荐人ID不合法",
+			FR:"Recom ID illégitime"
+		},
+		verifyCodeIll:{
+			CN:"推荐人验证码不合法",
+			FR:"Validat illégitime"
+		},
+		accountorverifyIll:{
+			CN:"推荐人ID或推荐人验证码不合法",
+			FR:"Recom ID or Validat illégitime"
+		},
+		loading:{
+			CN:"正在加载...",
+			FR:"Chargement..."
+		}
 	}
 }
 	
