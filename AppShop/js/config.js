afa8465ba5ba720320233f99e7dbd3806c5fcc53
lth1990118 <438168789@qq.com>
2019-09-02 23:29:58 +0800

    0

--- AppShop/js/config.js
+++ AppShop/js/config.js
@@ -3,9 +3,9 @@ var config = {
 // 	url: "http://wskmlapi.yzj1999.com/api/ws/",
 // 	urlPic: "http://wskmlapi.yzj1999.com/api/pic/",
 // 	urlPicRoot: "http://wskmlapi.yzj1999.com/Images/",
-		url:"http://wskmlapi.yizhijiu.com/api/ws/" ,
-		urlPic:"http://wskmlapi.yizhijiu.com/api/pic/" ,
-		urlPicRoot:"http://wskmlapi.yizhijiu.com/Images/" ,
+		url:"http://wstsnyapi.yizhijiu.com/api/ws/" ,
+		urlPic:"http://wstsnyapi.yizhijiu.com/api/pic/" ,
+		urlPicRoot:"http://wstsnyapi.yizhijiu.com/Images/" ,
 	reqSys: 'FR'
 }
 
