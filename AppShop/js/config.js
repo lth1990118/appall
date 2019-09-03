0a59cbee4be579c94982bc5f79c597097203501b
lth1990118 <438168789@qq.com>
2019-09-03 23:54:23 +0800

    20190903

--- AppShop/js/config.js
+++ AppShop/js/config.js
@@ -1,11 +1,9 @@
 var config = {
-
-// 	url: "http://wskmlapi.yzj1999.com/api/ws/",
-// 	urlPic: "http://wskmlapi.yzj1999.com/api/pic/",
-// 	urlPicRoot: "http://wskmlapi.yzj1999.com/Images/",
-		url:"http://wstsnyapi.yizhijiu.com/api/ws/" ,
-		urlPic:"http://wstsnyapi.yizhijiu.com/api/pic/" ,
-		urlPicRoot:"http://wstsnyapi.yizhijiu.com/Images/" ,
-	reqSys: 'FR'
+	//url: "http://wstsnyapi.yizhijiu.com/api/ws/",
+	//urlPic: "http://wstsnyapi.yizhijiu.com/api/pic/",
+	//urlPicRoot: "http://wstsnyapi.yizhijiu.com/Images/",
+	url: "http://wstsnyapi.yizhijiu.com/api/ws/",
+	urlPic: "http://wstsnyapi.yizhijiu.com/api/pic/",
+	urlPicRoot: "http://wstsnyapi.yizhijiu.com/Images/",
+	reqSys: 'CN'
 }
-
