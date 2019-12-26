3289ef4f078a9fdfba673629f826ff08a1312917
lth1990118 <438168789@qq.com>
2019-12-26 22:18:58 +0800

    201912226

--- AppShop/js/config.js
+++ AppShop/js/config.js
@@ -3,9 +3,9 @@ var config = {
 // 	url: "http://wskmlapi.yzj1999.com/api/ws/",
 // 	urlPic: "http://wskmlapi.yzj1999.com/api/pic/",
 // 	urlPicRoot: "http://wskmlapi.yzj1999.com/Images/",
-		url:"http://wstsnyapi.yizhijiu.com/api/ws/" ,
-		urlPic:"http://wstsnyapi.yizhijiu.com/api/pic/" ,
-		urlPicRoot:"http://wstsnyapi.yizhijiu.com/Images/" ,
-	reqSys: 'EN'
+		url:"http://wsggjapi.yizhijiu.com/api/ws/" ,
+		urlPic:"http://wsggjapi.yizhijiu.com/api/pic/" ,
+		urlPicRoot:"http://wsggjapi.yizhijiu.com/Images/" ,
+	reqSys: 'FR'
 }
 
